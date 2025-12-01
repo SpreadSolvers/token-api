61682f3298447f0bbda9d2efa06d655673c2c346
0xcompose <0xcompose@maat.finance>
2025-12-01 13:24:50 +0300

    fix(migrations): add constraints for evm_tokens table fields to ensure data integrity

--- migrations/2025-11-28-143456-0000_evm_tokens/up.sql
+++ migrations/2025-11-28-143456-0000_evm_tokens/up.sql
@@ -1,9 +1,9 @@
 -- Your SQL goes here
 CREATE TABLE evm_tokens (
     id VARCHAR(255) NOT NULL PRIMARY KEY,
-    chain_id INT NOT NULL,
-    address VARCHAR(255) NOT NULL,
+    chain_id INT NOT NULL CHECK (chain_id > 0),
+    address VARCHAR(255) NOT NULL CHECK (LENGTH(address) = 42),
     symbol VARCHAR(255) NOT NULL,
-    decimals TINYINT NOT NULL,
+    decimals INT NOT NULL CHECK (decimals BETWEEN 0 AND 255),
     name VARCHAR(255) NOT NULL
 );
