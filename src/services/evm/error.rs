e5e42f84058f90c7c7174d5ebd2d06d57fce472e
0xcompose <0xcompose@maat.finance>
2026-03-27 18:36:37 +0300

    refactor(error): implement thiserror for EvmTokenServiceError to enhance error handling

--- src/services/evm/error.rs
+++ src/services/evm/error.rs
@@ -1,29 +1,34 @@
-use std::{
-    error::Error,
-    fmt::{self, Display},
-};
-
 use crate::repositories::RepoError;
 use actix_web::error::BlockingError;
 use alloy::transports::TransportError;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum EvmTokenServiceError {
+    #[error("Repository error: {0}")]
     Repository(RepoError),
+
+    #[error("Chain error: {0}")]
     Chain(TransportError),
+
+    #[error("Multicall error: {0}")]
     Multicall(String),
+
+    #[error("Chain ID mismatch: {0} != {1}")]
     ChainIdMismatch(u64, u64),
+
+    #[error("CAIP ID build failed: {0}")]
     CaipIdBuildFailed(tap_caip::error::Error),
+
+    #[error("Blocking error: {0}")]
     BlockingError(BlockingError),
 }
 
-impl Display for EvmTokenServiceError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "EvmTokenServiceError: {}", self)
-    }
-}
-
-impl Error for EvmTokenServiceError {}
+// impl Display for EvmTokenServiceError {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f, "EvmTokenServiceError: {}", self)
+//     }
+// }
 
 impl From<RepoError> for EvmTokenServiceError {
     fn from(error: RepoError) -> Self {
